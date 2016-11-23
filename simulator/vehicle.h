675806efee49ec3043dab9418f4d08035c7ea29d
Mihai Litu <mihai.litu@trendlee.com>
2016-11-23 10:06:44 +0200

    Rename car to vehicle

---
+++ simulator/vehicle.h
@@ -0,0 +1,22 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+
+class vehicle
+{
+    /* the length of the car.
+     * We can have:
+     *      -
+     *      - compact car - usual sedan 3.5 - 5 meters long
+     *      - van - 7-8 meters long
+     *      - bus - 12 - 18 meters long
+     *      - truck - 20 -25 meters long
+     * The type of the vechicle should be determined trough statistics:
+     *      in a regular city, sedans (or shorter should be more probable than trucks, etc
+     */
+    int length;
+public:
+    vehicle();
+};
+
+#endif // VEHICLE_H
