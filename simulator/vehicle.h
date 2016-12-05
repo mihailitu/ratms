5a4c638c98a64ed13edf5a93410321336baf3acd
Mihai Litu <mihai.litu@trendlee.com>
2016-12-05 17:50:51 +0200

    Improve comments

--- simulator/vehicle.h
+++ simulator/vehicle.h
@@ -6,12 +6,12 @@ class vehicle
 {
     /* the length of the car.
      * We can have:
-     *      -
      *      - compact car - usual sedan 3.5 - 5 meters long
      *      - van - 7-8 meters long
      *      - bus - 12 - 18 meters long
      *      - truck - 20 -25 meters long
      * The type of the vechicle should be determined trough statistics:
+     *      there should be some statistics with vehicle type distribution to a city - from the Cityhall
      *      in a regular city, sedans (or shorter should be more probable than trucks, etc
      */
     int length;
