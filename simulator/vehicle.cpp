1e64930fb78dc8ae6120bd07a694dc03bd7a5d3e
Mihai Litu <mihai.litu@trendlee.com>
2016-12-06 18:54:38 +0200

    Add constructor with params

--- simulator/vehicle.cpp
+++ simulator/vehicle.cpp
@@ -1,6 +1,7 @@
 #include "vehicle.h"
 
-vehicle::vehicle()
+Vehicle::Vehicle( int _x_orig, int _length ) :
+    x_orig(_x_orig), length(_length)
 {
 
 }
--- simulator/vehicle.h
+++ simulator/vehicle.h
@@ -2,7 +2,7 @@
 #define VEHICLE_H
 
 
-class vehicle
+class Vehicle
 {
     /* the length of the car.
      * We can have:
@@ -14,17 +14,15 @@ class vehicle
      *      there should be some statistics with vehicle type distribution to a city - from the Cityhall
      *      in a regular city, sedans (or shorter should be more probable than trucks, etc
      */
-    int length;
-    int current_speed;
 
-
-    /* desired velocity */
-    int v0;
-
-    // pos on the road. It will be updated through IDM
-    int x_pos;
+    int x_orig;     // when a vechicle is created, it has to start(appear) somewhere
+    int length;     // vechile length
+    int velocity;   // current velocity. It will be update through IDM
+    int x_pos;      // current position on the road. It will be updated through IDM
+    bool free_road; // if the distance to the vechicle on front is large,
+                    // we will not consider other vechicles to update current velocity and current position
 public:
-    vehicle();
+    Vehicle( int _length, int _x_orig );
 };
 
 #endif // VEHICLE_H
