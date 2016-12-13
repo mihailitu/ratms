ed727a750a94b2b949aeffd5737a0919579cbbd2
Mihai Litu <mihai.litu@trendlee.com>
2016-12-13 14:53:34 +0200

    Start building the road

--- simulator/road.h
+++ simulator/road.h
@@ -1,11 +1,51 @@
 #ifndef ROAD_H
 #define ROAD_H
 
+#include <utility>
 
 class Road
 {
+    /***
+     * A road is the one-way section between two semaphores. If a real road (from OMS or else) determines that
+     * a road has two ways, we will tread those separately.
+     *
+     * It's length is expressed in meters and it will behave like x-axis for vechicle position
+     *
+     * It is characterized by GPS coordinates: start - end coordinates show us the direction of the traffic flow.
+     * These coordinates will be also used to represent the road on a map (GUI, etc).
+     *
+     * A road has connections to other roads: where the traffic can go from this road.
+     * Each connection will have a probability that a car should choose it taken from an external and real statistic.
+     * Busier roads will have a higher probability that a car will choose that road.
+     *
+     * Also, based on real statistics, cars might appear/disappear from roads, to temper with real city traffic statistics.
+     * The point is: while we only know the number of cars at the sempahore points, the inner roads (residential roads, smaller entrances) will not be monitored.
+     * Some cars might be leaving home (enter this road), some might go home/work/shop etc. (exiting this road)
+     */
+
+    // road ID - OMS related.
+    // TODO - this ID could be duplicated in the case of two way roads. Maybe we should have two id's: OMS id and internal ID
+    unsigned long id;
+
+    // length of the road in meters
+    int length;
+    // x_pos of vechicle is
+
+    // start position of the road - lat/lon
+    float startLat;
+    float startLon;
+
+    // end position of the road - lat/lon
+    float endLat;
+    float endLon;
+    // the traffic will flow from startPos to endPos
+
+    // the number of lanes
+    int lanes;
+
     // road max speed - if any or city speed limit
     int max_speed;
+
 public:
     Road();
 };
