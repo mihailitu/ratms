a12e8694bc0e3e1c2cb94f38942b186a60bd9d1a
Mihai Litu <mihai.litu@trendlee.com>
2017-01-30 14:17:57 +0200

    Chapter 2

---
+++ CrackingCodeInterview/chapter_2.cpp
@@ -0,0 +1,41 @@
+#include "chapter_2.h"
+
+#include <iostream>
+
+class Node {
+    Node *next = nullptr;
+    int data;
+
+public:
+    Node( int d ) : data( d ) {}
+
+    void appendToTail( int d ) {
+        Node *end = new Node(d);
+        Node *n = this;
+        while( n->next != nullptr )
+            n = n->next;
+
+        n->next = end;
+    }
+
+    Node* deleteNode(Node *head, int d) {
+        Node *n = head;
+
+        if (n->data == d)
+            return head->next;
+
+        while (n->next != nullptr) {
+            if (n->next->data == d) {
+                n->next = n->next->next;
+                return head;
+            }
+            n = n->next;
+        }
+        return head;
+    }
+};
+
+void run_chapter_2()
+{
+
+}
---
+++ CrackingCodeInterview/chapter_2.h
@@ -0,0 +1,7 @@
+#ifndef CHAPTER_2_H
+#define CHAPTER_2_H
+
+
+void run_chapter_2();
+
+#endif // CHAPTER_2_H
