57726655e54c1d1c7fbe1017247d76263c949f58
Mihai Litu <mihai.litu@trendlee.com>
2017-01-31 11:40:17 +0200

    Make list templetized in chapter two

--- CrackingCodeInterview/chapter_2.cpp
+++ CrackingCodeInterview/chapter_2.cpp
@@ -2,38 +2,61 @@
 
 #include <iostream>
 
-class Node {
-    Node *next = nullptr;
-    int data;
+template<class T>
+class LinkedList {
+    struct Node {
+        Node *next;
+        T *data;
+    };
+
+    Node *root;
 
 public:
-    Node( int d ) : data( d ) {}
+    LinkedList(T *root) {
 
-    void appendToTail( int d ) {
-        Node *end = new Node(d);
-        Node *n = this;
-        while( n->next != nullptr )
-            n = n->next;
-
-        n->next = end;
     }
 
-    Node* deleteNode(Node *head, int d) {
-        Node *n = head;
+    void insert(T *data) {
 
-        if (n->data == d)
-            return head->next;
+    }
 
-        while (n->next != nullptr) {
-            if (n->next->data == d) {
-                n->next = n->next->next;
-                return head;
-            }
-            n = n->next;
-        }
-        return head;
+    void remove(T *data) {
+
     }
 };
+
+//class Node {
+//    Node *next = nullptr;
+//    int data;
+
+//public:
+//    Node( int d ) : data( d ) {}
+
+//    void appendToTail( int d ) {
+//        Node *end = new Node(d);
+//        Node *n = this;
+//        while( n->next != nullptr )
+//            n = n->next;
+
+//        n->next = end;
+//    }
+
+//    Node* deleteNode(Node *head, int d) {
+//        Node *n = head;
+
+//        if (n->data == d)
+//            return head->next;
+
+//        while (n->next != nullptr) {
+//            if (n->next->data == d) {
+//                n->next = n->next->next;
+//                return head;
+//            }
+//            n = n->next;
+//        }
+//        return head;
+//    }
+//};
 
 void run_chapter_2()
 {
--- CrackingCodeInterview/chapter_2.h
+++ CrackingCodeInterview/chapter_2.h
@@ -1,7 +1,6 @@
 #ifndef CHAPTER_2_H
 #define CHAPTER_2_H
 
-
 void run_chapter_2();
 
 #endif // CHAPTER_2_H
