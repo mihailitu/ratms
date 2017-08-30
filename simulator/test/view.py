cac966218d610f19f447482fc0bed1913965e146
Mihai Litu <litu_mihai@yahoo.com>
2017-08-30 13:34:02 +0300

    first working view

--- simulator/test/view.py
+++ simulator/test/view.py
@@ -4,41 +4,34 @@ import matplotlib.animation as animation
 
 ## TODO: see https://matplotlib.org/examples/animation/rain.html for scatter plots
 
-def update_line(num, data, line):
-    print "update_line"
-    data[0][0] = data[0][0] + 0.01
-    line.set_data(data)
-    return line,
-
-fig = plt.figure(figsize=(15, 3))
-ax = fig.add_axes([0, 0, 1, 1], frameon=False)
-ax.set_xlim(0, 1), ax.set_xticks([])
-ax.set_ylim(0, 1), ax.set_yticks([])
-
 # no of cars
 N = 2
-
+road_length = 5000
+# update interval same as dt from simulation ODE = 0.5 sec
+interval = 500
+# vehicles data
 cars = np.zeros(N, dtype=[('position', float, 2),
                           ('time', float, 1),
-                          ('speed', float, 1),
+                          ('velocity', float, 1),
                           ('acceleration', float, 1)])
 
-# x coord of each car
-X = np.zeros(N)
-# y coord of each car - stay on the middle of the screen
-Y = np.full(1, 0.5)
+# don't update y pos
+cars['position'][:, 1] = np.full((1, N), 0)
 
-#plot coordinates
-data = [X, Y]
-# print "data " + str(data)
-l, = plt.plot([], [], 'b>')
-# plt.show()
-# exit()
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-# plt.xlabel('x')
-# plt.title('test')
+# setup view
+fig = plt.figure(figsize=(15, 3))
+ax = plt.axes(xlim=(0, road_length), ylim=(-1, 1))
+scat = plt.scatter(cars['position'][:, 0], cars['position'][:, 1])
 
-line_ani = animation.FuncAnimation(fig, update_line, 25, fargs=(data, l),
-                                   interval=500, blit=True)
+
+def update(frame_no):
+    print 'update ' + str(frame_no)
+    cars['position'][0][0] += 10
+    cars['position'][1][0] += (frame_no % 2) * 10
+
+    scat.set_offsets(cars['position'])
+
+
+animation = animation.FuncAnimation(fig, update, interval=500)
+
 plt.show()
