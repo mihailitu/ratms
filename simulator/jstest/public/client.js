f7309256f69becc57520919ee645b9ddbc6ee027
Mihai Litu <mihai.litu@google.com>
2019-03-30 15:51:59 +0200

    Make vehicles visible

--- simulator/jstest/public/client.js
+++ simulator/jstest/public/client.js
@@ -1,97 +1,99 @@
-  document.addEventListener("DOMContentLoaded", function() {
-   var mouse = {
-      click: false,
-      move: false,
-      pos: {x:0, y:0},
-      pos_prev: false
-   };
-   // get canvas element and create context
-   document.title = "Bla bla";
-   var canvas  = document.getElementById('drawing');
-   var context = canvas.getContext('2d');
-   var viewWidth   = window.innerWidth - 10;
-   var viewHeight  = window.innerHeight - 10;
-   var socket  = io.connect();
+document.addEventListener("DOMContentLoaded", function() {
+    var mouse = {
+        click: false,
+        move: false,
+        pos: {x:0, y:0},
+        pos_prev: false
+    };
+    // get canvas element and create context
+    document.title = "Bla bla";
+    var canvas  = document.getElementById('drawing');
+    var context = canvas.getContext('2d');
+    var viewWidth   = window.innerWidth - 10;
+    var viewHeight  = window.innerHeight - 10;
+    var socket  = io.connect();
 
-   // set canvas to full browser width/height
-   canvas.width = window.innerWidth - 10;
-   canvas.height = window.innerHeight - 10;
+    // set canvas to full browser width/height
+    canvas.width = window.innerWidth - 10;
+    canvas.height = window.innerHeight - 10;
 
-   console.log("client canvas[" + canvas.width + ", " + canvas.height + "]");
-   console.log("client view[" + viewWidth + ", " + viewHeight + "]");
-   // register mouse event handlers
-   canvas.onmousedown = function(e){ mouse.click = true; };
-   canvas.onmouseup = function(e){ mouse.click = false; };
+    console.log("client canvas[" + canvas.width + ", " + canvas.height + "]");
+    console.log("client view[" + viewWidth + ", " + viewHeight + "]");
+    // register mouse event handlers
+    canvas.onmousedown = function(e){ mouse.click = true; };
+    canvas.onmouseup = function(e){ mouse.click = false; };
 
-   canvas.onmousemove = function(e) {
-      // normalize mouse position to range 0.0 - 1.0
-      mouse.pos.x = e.clientX / viewWidth;
-      mouse.pos.y = e.clientY / viewHeight;
-      mouse.move = true;
-   };
+    canvas.onmousemove = function(e) {
+        // normalize mouse position to range 0.0 - 1.0
+        mouse.pos.x = e.clientX / viewWidth;
+        mouse.pos.y = e.clientY / viewHeight;
+        mouse.move = true;
+    };
 
-   // save road map. It will not change through the run of the program
-  road_map = {};
+    function sleep(start, delay) {
+        // var start = new Date().getTime();
+        while (new Date().getTime() < start + delay);
+    }
+    // save road map. It will not change through the run of the program
+    road_map = {};
 
-   // draw line received from server
-	socket.on('draw_map', function (data) {
-    road_map = data.map;
-    console.log("Map loaded. Length: " + road_map.length)
-   });
+    // draw line received from server
+    socket.on('draw_map', function (data) {
+        road_map = data.map;
+        console.log("Map loaded. Length: " + road_map.length)
+    });
 
-   socket.on('draw_state', function (data) {
-     var canvas  = document.getElementById('drawing');
-     var context = canvas.getContext('2d');
+    socket.on('draw_state', function (data) {
+        var canvas  = document.getElementById('drawing');
+        var context = canvas.getContext('2d');
 
-     // clear canvas
-     context.clearRect(0, 0, viewWidth, viewHeight);
+        var delay = 500; //ms
+        var statFrameTime = new Date().getTime();
+        // clear canvas
+        context.clearRect(0, 0, viewWidth, viewHeight);
 
-     var delay = 500; //ms
-     var statFrameTime = new Date().getTime();
+        // first, draw the roads
+        for(var i = 0; i < road_map.length; i++) {
+            var road = road_map[i];
+            // console.log('drawing: ' + road.id + ': ' + JSON.stringify(road));
+            context.beginPath();
+            context.lineWidth = road.lanes * 3;
+            context.moveTo(road.start.x * viewWidth, road.start.y * viewHeight);
+            context.lineTo(road.end.x * viewWidth, road.end.y * viewHeight);
+            context.strokeStyle = "#eff2f7";
+            // context.strokeStyle = 'green';
+            context.stroke();
+        }
+        console.log("frame: " + data.time);
 
-     // first, draw the roads
-     for(var i = 0; i < road_map.length; i++) {
-       var road = road_map[i];
-       // console.log('drawing: ' + road.id + ': ' + JSON.stringify(road));
-       context.beginPath();
-       context.lineWidth = road.lanes * 3;
-       context.moveTo(road.start.x * viewWidth, road.start.y * viewHeight);
-       context.lineTo(road.end.x * viewWidth, road.end.y * viewHeight);
-       context.strokeStyle = "#eff2f7";
-       // context.strokeStyle = 'green';
-       context.stroke();
-     }
+        for(var i = 0; i < data.data.length; ++i) {
+            for(var v = 0; v < data.data[i].vehicles.length; ++v) {
+                context.beginPath();
+                context.arc(data.data[i].vehicles[v].x * viewWidth, data.data[i].vehicles[v].y * viewHeight, 3, 0, 2 * Math.PI);
+                context.fillStyle = "blue";
+                context.strokeStyle = "red";
+                context.lineWidth = 1;
+                context.stroke();
+                context.fill();
+            }
+        }
 
-     for(var i = 0; i < data.vehicles.length; ++i) {
-       context.beginPath();
-       context.arc(data.vehicles[i].x * viewWidth, data.vehicles[i].y * viewHeight, 3, 0, 2 * Math.PI);
-       context.fillStyle = "blue";
-       context.strokeStyle = "red";
-       context.lineWidth = 1;
-       context.stroke();
-       context.fill();
-     }
+        sleep(statFrameTime, delay);
 
-     function sleep(start, delay) {
-         // var start = new Date().getTime();
-         while (new Date().getTime() < start + delay);
-     }
-     sleep(statFrameTime, delay);
-
-     socket.emit('next_frame');
-   });
-   // main loop, running every 25ms
-   function mainLoop() {
-      // check if the user is drawing
-      // if (mouse.click && mouse.move && mouse.pos_prev) {
-      //    // send line to to the server
-      //    socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
-      //    mouse.move = false;
-      // }
-      // mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
-      setTimeout(mainLoop, 25);
-   }
-   // socket.emit('draw_line', {line: [{x: 0.0, y: 0.0}, {x: 0.5, y:0.5}]})
-   // socket.emit('draw_line', {line: [{x: 0.5, y: 0.5}, {x: 1.0, y:0}]})
-   mainLoop();
+        socket.emit('next_frame');
+    });
+    // main loop, running every 25ms
+    function mainLoop() {
+        // check if the user is drawing
+        // if (mouse.click && mouse.move && mouse.pos_prev) {
+        //    // send line to to the server
+        //    socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
+        //    mouse.move = false;
+        // }
+        // mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+        setTimeout(mainLoop, 25);
+    }
+    // socket.emit('draw_line', {line: [{x: 0.0, y: 0.0}, {x: 0.5, y:0.5}]})
+    // socket.emit('draw_line', {line: [{x: 0.5, y: 0.5}, {x: 1.0, y:0}]})
+    mainLoop();
 });
--- simulator/jstest/server.js
+++ simulator/jstest/server.js
@@ -1,11 +1,11 @@
 var express = require('express'),
-app = express(),
-http = require('http'),
-socketIo = require('socket.io');
+    app = express(),
+    http = require('http'),
+    socketIo = require('socket.io');
 
 var lines = require('fs').readFileSync('roads.dat', 'utf-8')
-.split('\n')
-.filter(Boolean);
+    .split('\n')
+    .filter(Boolean);
 
 // Map of the roads, where the key is road_id
 // This structure will hold all road info and it will connect cars to roads
@@ -16,71 +16,120 @@ var roads = [];
 
 // roads_v2.dat
 // roadID0 | startLon | startLat | endLon | endLat | startX | startY | endX | endY | length | maxSpeed | lanes_no |
-for(var i in lines) {
-  var road_data = lines[i].split(' ');
+for (var i in lines) {
+    var road_data = lines[i].split(' ');
 
-  roads.push({
-    id: road_data[0],
-    lanes: road_data[11],
-    start: {
-      x: road_data[5],
-      y: road_data[6]
-    },
-    end: {
-      x: road_data[7],
-      y: road_data[8]
-    }
-  });
+    roads.push({
+        id: road_data[0],
+        lanes: road_data[11],
+        start: {
+            x: road_data[5],
+            y: road_data[6]
+        },
+        end: {
+            x: road_data[7],
+            y: road_data[8]
+        }
+    });
 
-  road_map[road_data[0]] =
-  {
-    startGeo: {
-      lat: road_data[1],
-      lon: road_data[2]
-    },
-    endGeo: {
-      lat: road_data[3],
-      lon: road_data[4]
-    },
-    startCard: {
-      x: road_data[5],
-      y: road_data[6],
-    },
-    endCard: {
-      x: road_data[7],
-      y: road_data[8],
-    },
-    length: road_data[9],
-    maxSpeed: road_data[10],
-    lanes: road_data[11]
-  };
+    road_map[road_data[0]] = {
+        startGeo: {
+            lat: road_data[1],
+            lon: road_data[2]
+        },
+        endGeo: {
+            lat: road_data[3],
+            lon: road_data[4]
+        },
+        startCard: {
+            x: Number(road_data[5]),
+            y: Number(road_data[6]),
+        },
+        endCard: {
+            x: Number(road_data[7]),
+            y: Number(road_data[8]),
+        },
+        length: road_data[9],
+        maxSpeed: road_data[10],
+        lanes: road_data[11]
+    };
 };
 
 // find out the dimensions of the map to normalize coordinates
 width = Math.max.apply(Math, roads.map(
-  function(o) {
-    return o.end.x;
-  }));
+    function(o) {
+        return o.end.x;
+    }));
 
 height = Math.max.apply(Math, roads.map(
-  function(o) {
-    return o.end.y;
-  }));
+    function(o) {
+        return o.end.y;
+    }));
 
-console.log('Map size: [' + width + ', ' + height +']');
+console.log('Map size: [' + width + ', ' + height + ']');
 
 // normalize coordinates
 roads.forEach(function(road) {
-  road.start.x = road.start.x / width;
-  road.start.y = road.start.y / height;
-  road.end.x = road.end.x / width;
-  road.end.y = road.end.y / height;
+    road.start.x = road.start.x / width;
+    road.start.y = road.start.y / height;
+    road.end.x = road.end.x / width;
+    road.end.y = road.end.y / height;
 });
 
 // read vehicle position
 var timeFrames = require('fs').readFileSync('output.dat', 'utf-8')
-.split('\n')
-.filter(Boolean);
+    .split('\n')
+    .filter(Boolean);
+
+function getCoordFromDist__(x1, y1, x2, y2, distance) {
+    // Determine line lengths
+    var xlen = x2 - x1;
+    var ylen = y2 - y1;
+
+    // Determine hypotenuse length
+    var hlen = Math.sqrt(Math.pow(xlen,2) + Math.pow(ylen,2));
+
+    // The variable identifying the length of the `shortened` line.
+    // In this case 50 units.
+    var smallerLen = distance;
+
+    // Determine the ratio between they shortened value and the full hypotenuse.
+    var ratio = smallerLen / hlen;
+
+    var smallerXLen = xlen * ratio;
+    var smallerYLen = ylen * ratio;
+
+    // The new X point is the starting x plus the smaller x length.
+    var smallerX = x1 + smallerXLen;
+
+    // Same goes for the new Y.
+    var smallerY = y1 + smallerYLen;
+    return {x: smallerX, y: smallerY};
+}
+
+function getCoordFromDist(startCoord, endCoord, distance) {
+    console.log({startCoord, endCoord});
+    // line lengths
+    var xlen = endCoord.x - startCoord.x;
+    var ylen = endCoord.y - startCoord.y;
+    console.log({xlen, ylen});
+    // hypotenuse length
+    var hlen = Math.sqrt(Math.pow(xlen,2) + Math.pow(ylen,2));
+    console.log({hlen});
+    // Determine the ratio between they ditance value and the full hypotenuse.
+    var ratio = distance / hlen;
+    console.log({ratio});
+    var newXLen = xlen * ratio;
+    var newYLen = ylen * ratio;
+    console.log({newXLen, newYLen});
+    // The new X point is the starting x plus the smaller x length.
+    var x = startCoord.x + newXLen;
+    // Same goes for the new Y.
+    var y = startCoord.y + newYLen;
+    console.log(startCoord.y + ' + ' + newYLen + ' = ' + y);
+    console.log({x: x, y: y});
+    return {x, y};
+}
 
 // each time frame
 var frames = [];
@@ -89,42 +138,49 @@ var prevFrameTime = 0.0;
 // all the roads involved in a timeframe
 var roadsForTime = [];
 
-for(var i in timeFrames) {
-  var frameData = timeFrames[i].split(' ');
-  var frameTime = frameData[0];
-  var data = {
-    roadID: frameData[1],
-    vehicles: []
-  };
+for (var i in timeFrames) {
+    var frameData = timeFrames[i].split(' ');
+    var frameTime = frameData[0];
+    var data = {
+        roadID: frameData[1],
+        vehicles: []
+    };
 
-  for(var v = 2; v < frameData.length; v+=4) {
-    data.vehicles.push({
-      // TODO: calculate the exact position of the vehicle relative
-      // to the road it belongs to
-      x: 0,
-      y: 0,
-      d: frameData[v],   // x: distance from the beginning of the road
-      v: frameData[v+1], // v: current velocity
-      a: frameData[v+2], // a: current acceleration
-      l: frameData[v+3] //  l: the lane that this vehicle belongs to
-    });
-  };
+    for (var v = 2; v < frameData.length; v += 4) {
+        console.log("Road: [" + JSON.stringify(road_map[data.roadID].startCard) + ', ' + JSON.stringify(road_map[data.roadID].endCard) + ']');
+        var vehicleCoord = getCoordFromDist(road_map[data.roadID].startCard, road_map[data.roadID].endCard, frameData[v]);
+        // var vehicleCoord = getCoordFromDist(road_map[data.roadID].startCard.x, road_map[data.roadID].startCard.y,
+        //                                     road_map[data.roadID].endCard.x, road_map[data.roadID].endCard.y,
+        //                                     frameData[v]);
+        console.log("VCoord: [" + JSON.stringify(vehicleCoord) + ']');
+        data.vehicles.push({
+            // TODO: calculate the exact position of the vehicle relative
+            // to the road it belongs to
+            x: vehicleCoord.x/width,
+            y: vehicleCoord.y/height,
+            d: frameData[v], // x: distance from the beginning of the road
+            v: frameData[v + 1], // v: current velocity
+            a: frameData[v + 2], // a: current acceleration
+            l: frameData[v + 3] //  l: the lane that this vehicle belongs to
+        });
+        console.log(JSON.stringify(data, null, 2));
+    };
 
-  if(prevFrameTime != frameTime) {
-    frames.push({
-      time: prevFrameTime,
-      data: roadsForTime,
-    });
-    roadsForTime = [];
-    prevFrameTime = frameTime;
-  }
+    if (prevFrameTime != frameTime) {
+        frames.push({
+            time: prevFrameTime,
+            data: roadsForTime,
+        });
+        roadsForTime = [];
+        prevFrameTime = frameTime;
+    }
 
-  roadsForTime.push(data);
+    roadsForTime.push(data);
 }
 console.log(JSON.stringify(frames, null, 2));
 
 // start webserver on port 8080
-var server =  http.createServer(app);
+var server = http.createServer(app);
 var io = socketIo.listen(server);
 server.listen(8080);
 
@@ -133,20 +189,22 @@ app.use(express.static(__dirname + '/public'));
 console.log("Server running on 127.0.0.1:8080");
 
 // event-handler for new incoming connections
-io.on('connection', function (socket) {
-  console.log("on connection: ");
+io.on('connection', function(socket) {
+    console.log("on connection: ");
 
-  socket.emit('draw_map', {map: roads});
+    socket.emit('draw_map', {
+        map: roads
+    });
 
-  var frame = 0;
-  // send vehicle status
-  socket.emit('draw_state', frames[frame++]);
-  // add handler for message type "draw_line".
-  socket.on('next_frame', function (data) {
-    if (frame < frames.length) {
-      socket.emit('draw_state', frames[frame++]);
-    } else {
-      console.log("DONE");
-    }
-  });
+    var frame = 0;
+    // send vehicle status
+    socket.emit('draw_state', frames[frame++]);
+    // add handler for message type "draw_line".
+    socket.on('next_frame', function(data) {
+        if (frame < frames.length) {
+            socket.emit('draw_state', frames[frame++]);
+        } else {
+            console.log("DONE");
+        }
+    });
 });
